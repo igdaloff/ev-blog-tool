1e6fd88e2a7d22e8720c898a23b7f7e74d1d91dd
igdaloff <louis.nathan@gmail.com>
2023-02-17 12:33:01 -0800

    better loading states for all buttons; scroll blog post section into view after title submit

--- src/views/CreateBlogPostView.vue
+++ src/views/CreateBlogPostView.vue
@@ -24,8 +24,8 @@
             </div>
           </div>
           <div class="flex justify-start mt-8">
-            <button type="submit" :disabled="!titlesLoading ? false : true" class="flex items-center rounded-md border border-transparent bg-sky-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">
-              {{ !titlesLoading ? 'Next' : 'Generating Titles' }}
+            <button type="submit" :disabled="titlesLoading ? true : false" class="flex items-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2" :class="titlesLoading ? 'bg-gray-400 cursor-default hover:bg-gray-400' : 'bg-sky-600 focus:ring-sky-500 hover:bg-sky-700'">
+              {{ titlesLoading ? 'Generating Titles...' : 'Next' }}
               <svg v-if="titlesLoading" aria-hidden="true" role="status" class="inline w-4 h-4 ml-2 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#ffffff" />
@@ -101,7 +101,7 @@
       </div> -->
 
       <!-- Step 4: View blog post output -->
-      <div v-show="bodyLoading || bodyOutput" class="relative bg-white px-4 py-5 mt-8 shadow sm:rounded-lg sm:p-6">
+      <div v-show="bodyLoading || bodyOutput" id="blog-post" class="relative bg-white px-4 py-5 mt-8 shadow sm:rounded-lg sm:p-6">
         <div v-if="bodyOutput" class="relative border-b border-gray-200 pb-5 sm:pb-0">
           <div class="md:flex md:items-center md:justify-between pb-4">
             <div class="flex ml-auto">
@@ -145,7 +145,13 @@
               </div>
             </div>
             <div class="flex justify-start mt-8">
-              <button type="submit" class="flex items-center rounded-md border border-transparent bg-sky-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">Submit and View Blog Post</button>
+              <button type="submit" :disabled="!emailSubmitting ? false : true" :class="emailSubmitting ? 'bg-gray-400 cursor-default hover:bg-gray-400' : 'bg-sky-600 hover:bg-sky-700 focus:ring-sky-500'" class="flex items-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2">
+                {{ !emailSubmitting ? 'Submit' : 'Loading...' }}
+                <svg v-if="emailSubmitting" aria-hidden="true" role="status" class="inline w-4 h-4 ml-2 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#ffffff" />
+                </svg>
+              </button>
             </div>
           </div>
         </form>
@@ -199,6 +205,7 @@ export default {
       titlesLoading: false,
       bodyLoading: false,
       bodyLoaded: false,
+      emailSubmitting: false,
       emailSubmitted: false,
       loadingBodyCopy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       dummyPosts: [
@@ -249,11 +256,19 @@ export default {
       this.bodyOutput = ''
       this.titleSelection = e.currentTarget.getAttribute('data-titleSelection').replace(/["]+/g, '')
 
+      // Scroll to blog post section
+      this.$nextTick(function () {
+        document.getElementById('blog-post').scrollIntoView({
+          behavior: 'smooth'
+        })
+      })
+
       // Get today's date for byline
       let dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
       let today = new Date()
       this.todaysDate = today.toLocaleDateString('en-US', dateOptions)
 
+      // Send prompt to OpenAI API
       const client = axios.create({
         headers: { Authorization: 'Bearer ' + this.apiKey }
       })
@@ -283,6 +298,7 @@ export default {
       const form = document.getElementById('emailForm')
       form.addEventListener('submit', (e) => {
         e.preventDefault()
+        this.emailSubmitting = true
         const data = new FormData(form)
         const action = e.target.action
         fetch(action, {
